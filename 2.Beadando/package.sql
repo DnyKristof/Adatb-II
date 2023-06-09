533f7333fbcb825301f732d8ad9788163038d5d1
Danyi <99331148+DnyKristof@users.noreply.github.com>
2023-06-10 00:27:36 +0200

    Fileos procedure

--- 2.Beadando/package.sql
+++ 2.Beadando/package.sql
@@ -15,3 +15,37 @@ EXCEPTION
     WHEN OTHERS THEN
         ROLLBACK;
 END;
+
+
+CREATE OR REPLACE PROCEDURE FILEBOL_ORVOS AS
+FAJL UTL_FILE.FILE_TYPE;
+LINE VARCHAR(200);
+COL INT;
+EMBER ORVOS%ROWTYPE;
+ID INT;
+BEGIN
+    FAJL:=UTL_FILE.FOPEN('TESZT','orvosok.txt','R');
+    LOOP
+        COL:=1;
+        UTL_FILE.GET_LINE(FAJL,LINE);
+        
+        --Szetkapkodjuk a sort vesszőnként ( [^,] minden karakter ami nem vessző , + ---> álljon meg egyből az első vesszőnél
+            FOR I IN (SELECT REGEXP_SUBSTR (LINE, '[^,]+', 1, level) AS STR FROM DUAL CONNECT BY REGEXP_SUBSTR(LINE, '[^,]+', 1, level) IS NOT NULL) LOOP
+            IF COL = 0 THEN
+                EMBER.Nev := I.STR;
+            ELSIF COL = 1 THEN
+                EMBER.Fizetes := TO_NUMBER(I.STR);
+            END IF;
+            COL:= COL+1;
+            SELECT FLOOR(DBMS_RANDOM.VALUE(1, 1001)) INTO ID FROM DUAL;
+            INSERT INTO ORVOS VALUES(ID,EMBER.Nev,EMBER.Fizetes);
+            
+            END LOOP;
+            
+        END LOOP;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        UTL_FILE.FCLOSE(FAJL);
+    WHEN OTHERS THEN
+        ROLLBACK;
+END;
